4dcdd6569cee1da5ee98d563986cf22e36257e37
TheCrudeOilBro <lucasyuan1981@gmail.com>
2024-01-21 14:34:13 -0500

    implement intake manager

---
+++ src/main/cpp/IntakeManager.cpp
@@ -0,0 +1,63 @@
+#include "IntakeManager.h"
+
+namespace
+{
+    const double NORMAL_INTAKE_SPEED = 0.8;
+    const double AMP_POS = 0.0;
+    const double GROUND_POS = 0.0;
+    const double STOW_POS = 0.0;
+}
+
+void IntakeManager::HandleInput(IntakeInput &input)
+{
+    if (input.runIntakeIn && !input.runIntakeOut)
+    {
+        m_intake.RunIntake(NORMAL_INTAKE_SPEED);
+    }
+    if (input.runIntakeOut && !input.runIntakeIn)
+    {
+        m_intake.RunIntake(-NORMAL_INTAKE_SPEED);
+    }
+    if (input.manualMove > 0.001)
+    {
+        m_intake.ManualMovePivot(input.manualMove);
+        m_intake.ResetProfiledMoveState();
+        m_goToAmpPos = false;
+        m_goToGroundPos = false;
+        m_goToStowPos = false;
+    }
+    else if (input.goToStowPos)
+    {
+        m_intake.ResetProfiledMoveState();
+        m_goToAmpPos = false;
+        m_goToGroundPos = false;
+        m_goToStowPos = true;
+    } 
+    else if (input.goToGroundPos)
+    {
+        m_intake.ResetProfiledMoveState();
+        m_goToAmpPos = false;
+        m_goToGroundPos = true;
+        m_goToStowPos = false;
+    }
+    else if (input.goToAmpPos)
+    {
+        m_intake.ResetProfiledMoveState();
+        m_goToAmpPos = true;
+        m_goToGroundPos = false;
+        m_goToStowPos = false;
+    }
+   
+    if (m_goToAmpPos)
+    {
+        m_intake.ProfiledMoveToAngle(AMP_POS);
+    }
+    if (m_goToGroundPos)
+    {
+        m_intake.ProfiledMoveToAngle(GROUND_POS);
+    }
+    if (m_goToStowPos)
+    {
+        m_intake.ProfiledMoveToAngle(STOW_POS);
+    }
+}
---
+++ src/main/include/IntakeManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "IntakeHAL.h"
+
+struct IntakeInput
+{
+    bool runIntakeOut;
+    bool runIntakeIn;
+    bool goToGroundPos;
+    bool goToStowPos;
+    bool goToAmpPos;
+    double manualMove;
+};
+
+class IntakeManager
+{
+
+public:
+    void HandleInput(IntakeInput &input);
+    
+private:
+    IntakeHAL m_intake;
+    bool m_goToGroundPos;
+    bool m_goToStowPos;
+    bool m_goToAmpPos;
+
+};
