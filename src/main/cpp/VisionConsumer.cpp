f8618736b9bfc157ea3a78a393c44dc7bd21f63b
JoeyBrar <jorabrar2007@gmail.com>
2024-02-08 11:21:59 -0500

    Get rest of vision shuffleboard info, add toPolar function, shorten code(not finished yet)

--- src/main/cpp/VisionConsumer.cpp
+++ src/main/cpp/VisionConsumer.cpp
@@ -1,9 +1,71 @@
 #include "VisionConsumer.h"
-#include <networktables/NetworkTableInstance.h>
 
-void VisionConsumer::Configure(VisionConsumerConfig &config) {
+VisionConsumer::VisionConsumer() {
     auto inst = nt::NetworkTableInstance::GetDefault();
     table = inst.GetTable("SmartDashboard")->GetSubTable("vision");
     aprilTagCoords = table->GetDoubleTopic("vision");
-    aprilTagVisible = table->GetBooleanTopic("vision");
-};
+    aprilTagVisible = table->GetBooleanTopic("vision");  
+}
+
+void VisionConsumer::Periodic() {
+
+    Apriltag3X.Get(-1.0);    
+    Apriltag3Y.Get(-1.0);
+    Apriltag3Z.Get(-1.0);    
+
+    Apriltag5X.Get(-1.0);
+    Apriltag5Y.Get(-1.0);
+    Apriltag5Z.Get(-1.0);
+
+    Apriltag6X.Get(-1.0);
+    Apriltag6Y.Get(-1.0);
+    Apriltag6Z.Get(-1.0);
+
+    Apriltag7X.Get(-1.0);
+    Apriltag7Y.Get(-1.0);
+    Apriltag7Z.Get(-1.0);
+
+    Apriltag3.Get();
+    Apriltag5.Get();
+    Apriltag6.Get();
+    Apriltag7.Get();
+
+    ApriltagCorrected3X.Get(-1.0);
+    ApriltagCorrected3Y.Get(-1.0);
+    ApriltagCorrected3Z.Get(-1.0);
+
+    ApriltagCorrected5X.Get(-1.0);
+    ApriltagCorrected5Y.Get(-1.0);
+    ApriltagCorrected5Z.Get(-1.0);
+
+    ApriltagCorrected6X.Get(-1.0);
+    ApriltagCorrected6Y.Get(-1.0);
+    ApriltagCorrected6Z.Get(-1.0);
+
+    ApriltagCorrected7X.Get(-1.0);
+    ApriltagCorrected7Y.Get(-1.0);
+    ApriltagCorrected7Z.Get(-1.0);
+
+    FPS.Get(-1.0);
+}
+
+void VisionConsumer::Subscribe() {
+
+    AprilTagSubscribers[3].Subscribe(table, 3);
+    AprilTagSubscribers[5].Subscribe(table, 5);
+    AprilTagSubscribers[6].Subscribe(table, 6);
+    AprilTagSubscribers[7].Subscribe(table, 7);
+
+
+    FPS = aprilTagCoords.Subscribe(0.0);
+}
+
+struct PolarCoords toPolar(double x, double y) {
+    double r = sqrt(pow(x, 2) + pow(y, 2));
+    double theta = atan2(y, x);
+
+    struct PolarCoords currentCoords;
+    currentCoords.r = r;
+    currentCoords.theta = theta;
+    return currentCoords; 
+}
--- src/main/include/VisionConsumer.h
+++ src/main/include/VisionConsumer.h
@@ -3,73 +3,103 @@
 #include <networktables/NetworkTableInstance.h>
 #include <networktables/DoubleTopic.h>
 #include <networktables/BooleanTopic.h>
+#include <map>
 
-struct VisionConsumerConfig {
-    nt::DoubleTopic aprilTagCoords;
-    nt::BooleanTopic aprilTagVisible;
+struct PolarCoords {
+    double r;
+    double theta;
+};
+
+struct XYZCoords {
+    double x;
+    double y;
+    double z;
+};
+
+struct XYZCoordsCurrent {
+    double x;
+    double y;
+    double z;
+};
+
+struct AprilTagSubscriber {
+    nt::DoubleSubscriber X;
+    nt::DoubleSubscriber Y;
+    nt::DoubleSubscriber Z;
+    nt::BooleanSubscriber visible;
+    nt::DoubleSubscriber correctedX;
+    nt::DoubleSubscriber correctedY;
+    nt::DoubleSubscriber correctedZ;
+
+    void Subscribe(std::shared_ptr<nt::NetworkTable> table, int id) {
+        X = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " X").Subscribe(0.0);
+        Y = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " Y").Subscribe(0.0);
+        Z = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " Z").Subscribe(0.0);
+        visible = table->GetBooleanTopic("Apriltag " + std::to_string(id)).Subscribe(false);
+        correctedX = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " X").Subscribe(0.0);
+        correctedY = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " Y").Subscribe(0.0);
+        correctedZ = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " Z").Subscribe(0.0);
+    }
+
+    XYZCoords GetXYZ() {
+         
+    }
+
+    XYZCoordsCurrent GetXYZCurrent() {
+
+    }
 };
 
 class VisionConsumer {
 
     public:
-        void Configure(VisionConsumerConfig &config);
+        VisionConsumer();
+        void Periodic();
+        void Subscribe();
+        struct PolarCoords toPolar(double x, double y);
+    
+    private:            
+        std::shared_ptr<nt::NetworkTable> table;
+        nt::DoubleTopic aprilTagCoords = table->GetDoubleTopic("vision");
+        nt::BooleanTopic aprilTagVisible = table->GetBooleanTopic("vision");
 
-        explicit VisionConsumer() {
+        nt::DoubleSubscriber Apriltag3X;
+        nt::DoubleSubscriber Apriltag3Y;
+        nt::DoubleSubscriber Apriltag3Z;
 
-            Apriltag5X = aprilTagCoords.Subscribe(0.0);
-            Apriltag5Y = aprilTagCoords.Subscribe(0.0);
-            Apriltag5Z = aprilTagCoords.Subscribe(0.0);
+        nt::DoubleSubscriber Apriltag5X;
+        nt::DoubleSubscriber Apriltag5Y;
+        nt::DoubleSubscriber Apriltag5Z;
 
-            Apriltag6X = aprilTagCoords.Subscribe(0.0);
-            Apriltag6Y = aprilTagCoords.Subscribe(0.0);
-            Apriltag6Z = aprilTagCoords.Subscribe(0.0);
+        nt::DoubleSubscriber Apriltag6X;
+        nt::DoubleSubscriber Apriltag6Y;
+        nt::DoubleSubscriber Apriltag6Z;
 
-            Apriltag7X = aprilTagCoords.Subscribe(0.0);
-            Apriltag7Y = aprilTagCoords.Subscribe(0.0);
-            Apriltag7Z = aprilTagCoords.Subscribe(0.0);
+        nt::DoubleSubscriber Apriltag7X;
+        nt::DoubleSubscriber Apriltag7Y;
+        nt::DoubleSubscriber Apriltag7Z;
 
-            Apriltag5 = aprilTagVisible.Subscribe(false);            
-            Apriltag6 = aprilTagVisible.Subscribe(false);
-            Apriltag7 = aprilTagVisible.Subscribe(false);            
-        } 
+        nt::BooleanSubscriber Apriltag3;
+        nt::BooleanSubscriber Apriltag5;
+        nt::BooleanSubscriber Apriltag6;
+        nt::BooleanSubscriber Apriltag7;
 
-        void Periodic() {
-            Apriltag5X.Get(-1.0);
-            Apriltag5Y.Get(-1.0);
-            Apriltag5Z.Get(-1.0);
+        nt::DoubleSubscriber ApriltagCorrected3X;
+        nt::DoubleSubscriber ApriltagCorrected3Y;
+        nt::DoubleSubscriber ApriltagCorrected3Z;
 
-            Apriltag6X.Get(-1.0);
-            Apriltag6Y.Get(-1.0);
-            Apriltag6Z.Get(-1.0);
+        nt::DoubleSubscriber ApriltagCorrected5X;
+        nt::DoubleSubscriber ApriltagCorrected5Y;
+        nt::DoubleSubscriber ApriltagCorrected5Z;
 
-            Apriltag7X.Get(-1.0);
-            Apriltag7Y.Get(-1.0);
-            Apriltag7Z.Get(-1.0);
+        nt::DoubleSubscriber ApriltagCorrected6X;
+        nt::DoubleSubscriber ApriltagCorrected6Y;
+        nt::DoubleSubscriber ApriltagCorrected6Z;
 
-            Apriltag5.Get();
-            Apriltag6.Get();
-            Apriltag7.Get();
-        }
+        nt::DoubleSubscriber ApriltagCorrected7X;
+        nt::DoubleSubscriber ApriltagCorrected7Y;
+        nt::DoubleSubscriber ApriltagCorrected7Z;
 
-        private:            
-            std::shared_ptr<nt::NetworkTable> table;
-            nt::DoubleTopic aprilTagCoords = table->GetDoubleTopic("vision");
-            nt::BooleanTopic aprilTagVisible = table->GetBooleanTopic("vision");
-
-            nt::DoubleSubscriber Apriltag5X;
-            nt::DoubleSubscriber Apriltag5Y;
-            nt::DoubleSubscriber Apriltag5Z;
-
-            nt::DoubleSubscriber Apriltag6X;
-            nt::DoubleSubscriber Apriltag6Y;
-            nt::DoubleSubscriber Apriltag6Z;
-
-            nt::DoubleSubscriber Apriltag7X;
-            nt::DoubleSubscriber Apriltag7Y;
-            nt::DoubleSubscriber Apriltag7Z;
-
-            nt::BooleanSubscriber Apriltag5;
-            nt::BooleanSubscriber Apriltag6;
-            nt::BooleanSubscriber Apriltag7;
-
+        nt::DoubleSubscriber FPS;
+        std::map<int, AprilTagSubscriber> AprilTagSubscribers;
 };
