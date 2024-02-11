1b91db7ddad3cc49a7e16a7677bb87d6dd68cd87
JoeyBrar <jorabrar2007@gmail.com>
2024-02-11 15:23:07 -0500

    Get all values from april tag

--- src/main/cpp/VisionConsumer.cpp
+++ src/main/cpp/VisionConsumer.cpp
@@ -5,59 +5,36 @@ VisionConsumer::VisionConsumer() {
     table = inst.GetTable("SmartDashboard")->GetSubTable("vision");
     aprilTagCoords = table->GetDoubleTopic("vision");
     aprilTagVisible = table->GetBooleanTopic("vision");  
-}
+    nt::DoubleSubscriber X;
+    nt::DoubleSubscriber Y;
+    nt::DoubleSubscriber Z;
+    nt::BooleanSubscriber visible;
+
+
+    for (int i = 0; i < 16; i++){
+
+        xSub.push_back(table->GetDoubleTopic("Apriltag " + std::to_string(i+1) + " X").Subscribe(0.0));
+        ySub.push_back(table->GetDoubleTopic("Apriltag " + std::to_string(i+1) + " y").Subscribe(0.0));
+        zSub.push_back(table->GetDoubleTopic("Apriltag " + std::to_string(i+1) + " z").Subscribe(0.0));        
+        vSub.push_back(table->GetBooleanTopic("Apriltag " + std::to_string(i+1) + "Detected").Subscribe(false));
+    
+    }
+    
+    FPS = table->GetDoubleTopic("FPS").Subscribe(0.0);
+}       
 
 void VisionConsumer::Periodic() {
+    
+    for (int i = 0; i<16; i++){
+        
+        x.push_back(xSub.at(i).Get());
+        y.push_back(ySub.at(i).Get());
+        z.push_back(zSub.at(i).Get());
+        v.push_back(vSub.at(i).Get());
+    }
 
-    Apriltag3X.Get(-1.0);    
-    Apriltag3Y.Get(-1.0);
-    Apriltag3Z.Get(-1.0);    
+    fps = FPS.Get();
 
-    Apriltag5X.Get(-1.0);
-    Apriltag5Y.Get(-1.0);
-    Apriltag5Z.Get(-1.0);
-
-    Apriltag6X.Get(-1.0);
-    Apriltag6Y.Get(-1.0);
-    Apriltag6Z.Get(-1.0);
-
-    Apriltag7X.Get(-1.0);
-    Apriltag7Y.Get(-1.0);
-    Apriltag7Z.Get(-1.0);
-
-    Apriltag3.Get();
-    Apriltag5.Get();
-    Apriltag6.Get();
-    Apriltag7.Get();
-
-    ApriltagCorrected3X.Get(-1.0);
-    ApriltagCorrected3Y.Get(-1.0);
-    ApriltagCorrected3Z.Get(-1.0);
-
-    ApriltagCorrected5X.Get(-1.0);
-    ApriltagCorrected5Y.Get(-1.0);
-    ApriltagCorrected5Z.Get(-1.0);
-
-    ApriltagCorrected6X.Get(-1.0);
-    ApriltagCorrected6Y.Get(-1.0);
-    ApriltagCorrected6Z.Get(-1.0);
-
-    ApriltagCorrected7X.Get(-1.0);
-    ApriltagCorrected7Y.Get(-1.0);
-    ApriltagCorrected7Z.Get(-1.0);
-
-    FPS.Get(-1.0);
-}
-
-void VisionConsumer::Subscribe() {
-
-    AprilTagSubscribers[3].Subscribe(table, 3);
-    AprilTagSubscribers[5].Subscribe(table, 5);
-    AprilTagSubscribers[6].Subscribe(table, 6);
-    AprilTagSubscribers[7].Subscribe(table, 7);
-
-
-    FPS = aprilTagCoords.Subscribe(0.0);
 }
 
 struct PolarCoords toPolar(double x, double y) {
--- src/main/include/VisionConsumer.h
+++ src/main/include/VisionConsumer.h
@@ -3,6 +3,7 @@
 #include <networktables/NetworkTableInstance.h>
 #include <networktables/DoubleTopic.h>
 #include <networktables/BooleanTopic.h>
+#include <vector>
 #include <map>
 
 struct PolarCoords {
@@ -14,40 +15,17 @@ struct XYZCoords {
     double x;
     double y;
     double z;
-};
+    double xCorrected;
+    double yCorrected;
+    double zCorrected;
+    double id;
+    std::vector<nt::DoubleSubscriber> GetXYZCoords;
 
-struct XYZCoordsCurrent {
-    double x;
-    double y;
-    double z;
-};
-
-struct AprilTagSubscriber {
-    nt::DoubleSubscriber X;
-    nt::DoubleSubscriber Y;
-    nt::DoubleSubscriber Z;
-    nt::BooleanSubscriber visible;
-    nt::DoubleSubscriber correctedX;
-    nt::DoubleSubscriber correctedY;
-    nt::DoubleSubscriber correctedZ;
-
-    void Subscribe(std::shared_ptr<nt::NetworkTable> table, int id) {
-        X = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " X").Subscribe(0.0);
-        Y = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " Y").Subscribe(0.0);
-        Z = table->GetDoubleTopic("Apriltag " + std::to_string(id) + " Z").Subscribe(0.0);
-        visible = table->GetBooleanTopic("Apriltag " + std::to_string(id)).Subscribe(false);
-        correctedX = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " X").Subscribe(0.0);
-        correctedY = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " Y").Subscribe(0.0);
-        correctedZ = table->GetDoubleTopic("ApriltagCorrected " + std::to_string(id) + " Z").Subscribe(0.0);
+    XYZCoords GetXYZ(int id) {
+        
+      
     }
 
-    XYZCoords GetXYZ() {
-         
-    }
-
-    XYZCoordsCurrent GetXYZCurrent() {
-
-    }
 };
 
 class VisionConsumer {
@@ -63,43 +41,16 @@ class VisionConsumer {
         nt::DoubleTopic aprilTagCoords = table->GetDoubleTopic("vision");
         nt::BooleanTopic aprilTagVisible = table->GetBooleanTopic("vision");
 
-        nt::DoubleSubscriber Apriltag3X;
-        nt::DoubleSubscriber Apriltag3Y;
-        nt::DoubleSubscriber Apriltag3Z;
-
-        nt::DoubleSubscriber Apriltag5X;
-        nt::DoubleSubscriber Apriltag5Y;
-        nt::DoubleSubscriber Apriltag5Z;
-
-        nt::DoubleSubscriber Apriltag6X;
-        nt::DoubleSubscriber Apriltag6Y;
-        nt::DoubleSubscriber Apriltag6Z;
-
-        nt::DoubleSubscriber Apriltag7X;
-        nt::DoubleSubscriber Apriltag7Y;
-        nt::DoubleSubscriber Apriltag7Z;
-
-        nt::BooleanSubscriber Apriltag3;
-        nt::BooleanSubscriber Apriltag5;
-        nt::BooleanSubscriber Apriltag6;
-        nt::BooleanSubscriber Apriltag7;
-
-        nt::DoubleSubscriber ApriltagCorrected3X;
-        nt::DoubleSubscriber ApriltagCorrected3Y;
-        nt::DoubleSubscriber ApriltagCorrected3Z;
-
-        nt::DoubleSubscriber ApriltagCorrected5X;
-        nt::DoubleSubscriber ApriltagCorrected5Y;
-        nt::DoubleSubscriber ApriltagCorrected5Z;
-
-        nt::DoubleSubscriber ApriltagCorrected6X;
-        nt::DoubleSubscriber ApriltagCorrected6Y;
-        nt::DoubleSubscriber ApriltagCorrected6Z;
-
-        nt::DoubleSubscriber ApriltagCorrected7X;
-        nt::DoubleSubscriber ApriltagCorrected7Y;
-        nt::DoubleSubscriber ApriltagCorrected7Z;
-
         nt::DoubleSubscriber FPS;
-        std::map<int, AprilTagSubscriber> AprilTagSubscribers;
+        std::vector<nt::DoubleSubscriber> xSub; 
+        std::vector<nt::DoubleSubscriber> ySub; 
+        std::vector<nt::DoubleSubscriber> zSub;
+        std::vector<nt::BooleanSubscriber> vSub; 
+        std::vector<double> x; 
+        std::vector<double> y; 
+        std::vector<double> z; 
+        std::vector<bool> v;
+        double fps;
+
+        std::map<int, XYZCoords> GetXYZCoords;
 };
