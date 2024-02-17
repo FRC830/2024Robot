4d3fe1c59844986916c20e390f12e07f91dded1a
TheCrudeOilBro <lucasyuan1981@gmail.com>
2024-02-16 20:11:42 -0500

    Fixed climbermanager math again :)

--- src/main/cpp/subsystems/subsystemManagers/ClimberManager.cpp
+++ src/main/cpp/subsystems/subsystemManagers/ClimberManager.cpp
@@ -9,15 +9,15 @@ void ClimberManager::HandleInput(ClimberInput &input)
 {
     
     // FIXME: I'm not correct according to the laws of mathematics
-    double controllerInput = ((input.controllerSpeed + m_deadzone)/(1-m_deadzone)) * m_multiplier;
+    double controllerInput = ((std::fabs(input.controllerSpeed) - m_deadzone)/(1-m_deadzone)) * m_multiplier;
     
     if (input.controllerSpeed > m_deadzone) 
     {
         m_climber.RunClimber(controllerInput);
     }
-    else if (input.controllerSpeed < m_deadzone)
+    else if (input.controllerSpeed < -(m_deadzone))
     {       
-        m_climber.RunClimber(((input.controllerSpeed - m_deadzone)/(1-m_deadzone)) * m_multiplier);
+        m_climber.RunClimber(-controllerInput);
     }
     else 
     {
