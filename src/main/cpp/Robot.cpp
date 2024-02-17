f35c96329e587d0a03165155ee4b295cc9710349
TheCrudeOilBro <lucasyuan1981@gmail.com>
2024-02-16 22:13:16 -0500

    add swerve drive command into teleop periodic

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -62,8 +62,8 @@ void Robot::TeleopInit() {}
 
 void Robot::TeleopPeriodic() {
   PrintSwerveInfo();
-  // _swerve.Drive(_controller.GetLeftY(), _controller.GetLeftX(), -_controller.GetRightX());
   _controller_interface.UpdateRobotControlData(_robot_control_data);
+  _swerve.Drive(_robot_control_data.swerveInput.xTranslation, _robot_control_data.swerveInput.yTranslation, _robot_control_data.swerveInput.rotation);
   _smart_intake.HandleInput(_robot_control_data);
   _intake_manager.HandleInput(_robot_control_data.intakeInput, _robot_control_data.intakeOutput);
 }
