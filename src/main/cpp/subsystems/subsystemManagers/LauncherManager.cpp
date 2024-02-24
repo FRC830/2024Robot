1448152933eb3701745968b5e233d1d2093fb08b
NottheIRS <92690588+NottheIRS@users.noreply.github.com>
2024-02-23 20:19:37 -0500

    Update sub angle and flywheel speed.

--- src/main/cpp/subsystems/subsystemManagers/LauncherManager.cpp
+++ src/main/cpp/subsystems/subsystemManagers/LauncherManager.cpp
@@ -2,9 +2,9 @@
 
 namespace
 {
-    const double SUB_ANGLE = 19.0;
+    const double SUB_ANGLE = 55.0;
     const double STOW_ANGLE = 10.0;
-    const double SUB_SPEED = 350.0;
+    const double SUB_SPEED = 150.0;
     const double INDEXER_SPEED = 1.0;
 }
 
